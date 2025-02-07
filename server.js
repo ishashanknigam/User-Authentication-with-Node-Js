b455b5e1171962dd300943a120a7842fcaf9442b
shashank <shashanknigamunnao@gmail.com>
2025-02-07 23:33:10 +0530

    initialize project

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./database/db');
+const authRoutes = require('./routes/auth-routes')
+const homeRoutes = require('./routes/home-routes')
+const adminRoutes = require('./routes/admin-routes')
 
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000
+
+//middleware
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/home', homeRoutes)
+app.use('/api/admin', adminRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is started on port ${PORT}`);
